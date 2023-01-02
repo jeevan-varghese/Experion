d87dc19b73a2349c7574a4e7a556801752a44a0f
jeevan-varghese <jeevanvarghese6235@gmail.com>
2023-01-02 22:18:58 +0530

    lab array

---
+++ Lab/JS/Array-length.js
@@ -0,0 +1,6 @@
+let array1 = [1, 2, 3, 4, 5];
+let count = 0;
+array1.forEach(element => {
+    count++;
+});
+console.log(count);
---
+++ Lab/JS/Array-sort-number.js
@@ -0,0 +1,12 @@
+let array = [44, 99, 22, 11, 77, 55, 33];
+
+for (let i = 0; i < array.length; i++) {
+  for (let j = i + 1; j < array.length; j++) {
+    if (array[i] > array[j]) {
+      temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
+}
+console.log("Sorted array=>", array);
